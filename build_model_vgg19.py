4e5e3ebca71eff3af57f0762270e4e73c83cc46c
MattBUWM <58745274+MattBUWM@users.noreply.github.com>
2022-03-29 15:56:42 +0200

    vgg11_v1.1

--- build_model_vgg19.py
+++ build_model_vgg19.py
@@ -13,97 +13,80 @@ validation_data_dir = 'dataset/validation'
 nb_train_samples = 500
 nb_validation_samples = 100
 epochs = 10
-batch_size = 50
-with tf.device('/cpu:0'):
-    if K.image_data_format() == 'channels_first':
-        input_shape = (3, img_width, img_height)
-    else:
-        input_shape = (img_width, img_height, 3)
+batch_size = 25
+if K.image_data_format() == 'channels_first':
+    input_shape = (3, img_width, img_height)
+else:
+    input_shape = (img_width, img_height, 3)
 
-    train_datagen = ImageDataGenerator(
-        rescale=1. / 255,
-        shear_range=0.2,
-        zoom_range=0.2,
-        horizontal_flip=True
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True
     )
 
-    test_datagen = ImageDataGenerator(rescale=1. / 255)
+test_datagen = ImageDataGenerator(rescale=1. / 255)
 
-    train_generator = train_datagen.flow_from_directory(
-        train_data_dir,
-        target_size=(img_width, img_height),
-        batch_size=batch_size,
-        class_mode='binary'
-    )
+train_generator = train_datagen.flow_from_directory(
+    train_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary'
+)
 
-    validation_generator = test_datagen.flow_from_directory(
-        validation_data_dir,
-        target_size=(img_width, img_height),
-        batch_size=batch_size,
-        class_mode='binary'
-    )
+validation_generator = test_datagen.flow_from_directory(
+    validation_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary'
+)
 
-    model = Sequential([
-        Conv2D(64, kernel_size=(3, 3), padding='same',
-               activation='relu', input_shape=input_shape),
-        Conv2D(64, kernel_size=(3, 3), padding='same',
-               activation='relu'),
-        MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
+model = Sequential([
+    Conv2D(64, kernel_size=(3, 3), padding='same',
+           activation='relu', input_shape=input_shape),
+    MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
 
-        Conv2D(128, kernel_size=(3, 3), padding='same',
-               activation='relu'),
-        Conv2D(128, kernel_size=(3, 3), padding='same',
-               activation='relu'),
-        MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
+    Conv2D(128, kernel_size=(3, 3), padding='same',
+           activation='relu'),
+    MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
 
-        Conv2D(256, kernel_size=(3, 3), padding='same',
-               activation='relu'),
-        Conv2D(256, kernel_size=(3, 3), padding='same',
-               activation='relu'),
-        Conv2D(256, kernel_size=(3, 3), padding='same',
-               activation='relu'),
-        Conv2D(256, kernel_size=(3, 3), padding='same',
-               activation='relu'),
-        MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
+    Conv2D(256, kernel_size=(3, 3), padding='same',
+           activation='relu'),
+    Conv2D(256, kernel_size=(3, 3), padding='same',
+           activation='relu'),
+    MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
 
-        Conv2D(512, kernel_size=(3, 3), padding='same',
-               activation='relu'),
-        Conv2D(512, kernel_size=(3, 3), padding='same',
-               activation='relu'),
-        Conv2D(512, kernel_size=(3, 3), padding='same',
-               activation='relu'),
-        Conv2D(512, kernel_size=(3, 3), padding='same',
-               activation='relu'),
-        MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
+    Conv2D(512, kernel_size=(3, 3), padding='same',
+           activation='relu'),
+    Conv2D(512, kernel_size=(3, 3), padding='same',
+           activation='relu'),
+    MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
 
-        Conv2D(512, kernel_size=(3, 3), padding='same',
-               activation='relu'),
-        Conv2D(512, kernel_size=(3, 3), padding='same',
-               activation='relu'),
-        Conv2D(512, kernel_size=(3, 3), padding='same',
-               activation='relu'),
-        Conv2D(512, kernel_size=(3, 3), padding='same',
-               activation='relu'),
-        MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
+    Conv2D(512, kernel_size=(3, 3), padding='same',
+           activation='relu'),
+    Conv2D(512, kernel_size=(3, 3), padding='same',
+           activation='relu'),
+    MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
 
-        Flatten(),
-        Dense(4096, activation='relu'),
-        Dropout(0.5),
-        Dense(4096, activation='relu'),
-        Dropout(0.5),
-        Dense(1000, activation='softmax'),
-    ])
+    Flatten(),
+    Dense(4096, activation='relu'),
+    Dropout(0.5),
+    Dense(4096, activation='relu'),
+    Dropout(0.5),
+    Dense(5, activation='softmax'),
+])
 
-    model.compile(
+model.compile(
         optimizer=tf.keras.optimizers.Adam(0.003),
         loss='sparse_categorical_crossentropy',
         metrics=['accuracy']
-    )
+)
 
-    model.fit(
+model.fit(
         train_generator,
         epochs=epochs,
         validation_data=validation_generator,
-    )
+)
 
-    model.save("vgg19")
+model.save("vgg11")
