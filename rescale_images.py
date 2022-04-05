f7eee703d3b77892e184757a36005407b94499b3
MattBUWM <58745274+MattBUWM@users.noreply.github.com>
2022-04-05 21:57:54 +0200

    improvement to rescale_images.py to also rescale (and remove transparency if present) .png images

--- rescale_images.py
+++ rescale_images.py
@@ -24,6 +24,21 @@ def rescale_images(input_folder, output_folder):
                 image = image.convert("RGB")
                 print(os.path.join(output_folder, class_folder, filename))
                 image.save(os.path.join(output_folder, class_folder, filename))
+            elif filename.endswith('.png'):
+                # print('Processing {}...'.format(filename))
+                image = Image.open(os.path.join(input_folder, class_folder, filename))
+                if image.mode == "RGBA":
+                    new_image = Image.new("RGB",output_resolution, (255, 255, 255))
+                    new_image.paste(image, image)
+                    new_image = resizeimage.resize_contain(new_image, output_resolution)
+                    print(os.path.join(output_folder, class_folder, filename))
+                    new_image.save(os.path.join(output_folder, class_folder, filename))
+                else:
+                    image = resizeimage.resize_contain(image, output_resolution)
+                    image = image.convert("RGB")
+                    print(os.path.join(output_folder, class_folder, filename))
+                    image.save(os.path.join(output_folder, class_folder, filename))
+
 
 rescale_images(input_folder, output_folder)
 
